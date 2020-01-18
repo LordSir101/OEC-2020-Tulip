b25d3f9012ccc18de078fbf8062f877422242ecd
LordSir101 <49959269+LordSir101@users.noreply.github.com>
2020-01-18 02:39:24 -0500

    Add files via upload

--- calculations.py
+++ calculations.py
@@ -3,26 +3,31 @@ from flask import(
 )
 import pandas as pd
 import numpy as np
+import time
+#from calculations import currentHour
 bp = Blueprint('calculations', __name__)
 
-@bp.route('/')
-def index():
 
-	currentHour = 0
+global currentHour 
+currentHour = 0
+
+global previousNuc
+previousNuc = 0
+@bp.route('/', methods=('GET', 'POST'))
+def index():
+	global currentHour 
+	global previousNuc
+
+	error = ""
+	
 	Starting = []
 	current = []
 	df1 = pd.read_csv('C:/Users/100707158/Documents/Visual Studio 2019/Oec 2020/inputFile.csv')
 	arr = df1.to_numpy()
 
 	#skip the first three arrays scince they are initial values
-	previousNuc = arr[1][4]
+	initial = arr[1][4]
 	current = arr[currentHour + 3]
-	#for x in arr:
-		#if x[0] ==0:
-			#Starting.append(x)
-		#else:
-			#current = x
-			#break
 
 
 	Energyarr = current[3:10]
@@ -31,7 +36,13 @@ def index():
 	colvals = ["Solar","Nuclear","Wind","Hydro","Coal","Biofuel","Neighbor"]
 
 	energyUsable= dict(zip(colvals,Energyarr))
-	energyUsable["Nuclear"] = previousNuc
+
+	if currentHour == 0:
+		energyUsable["Nuclear"] = initial
+		previousNuc = energyUsable["Nuclear"]
+		
+	else:
+		energyUsable["Nuclear"] = previousNuc
 
 	#energy used in order of least to most CO2 production
 	energyUsed = {
@@ -47,7 +58,6 @@ def index():
 	for key in energyUsed:
 		energyUsed[key] = energyUsable[key]
 		used = get_energy_used(energyUsed)
-	#energyUsed["Hydro"] = energyUsable["Hydro"]
 	
 		if used > demand:
 			difference = used - demand
@@ -55,11 +65,68 @@ def index():
 			used = get_energy_used(energyUsed)
 			break
 
-	return render_template('index.html', display = energyUsed, usable = energyUsable, demand = demand, used = used)
+	#totalCost = calculatecost(energyUsed)
+	
+	currentHour += 1
+	
+		
+
+	return render_template('index.html', display = energyUsed, usable = energyUsable, demand = demand, used = used, error = error)
 
 def get_energy_used(used_dict):
 	total = 0
+
 	for value in used_dict.values():
-		total += int(value)
+		total += float(value)
 
 	return total
+
+def calculate_cost(energy):
+    result = 0
+    
+
+    ts = energy['Solar'] * cost
+    result += ts/1000
+    ns = energy['Nuclear'] * cost
+    result += ns/1000
+    hs = energy['Hydro'] * cost
+    result += hs/1000
+    gs = energy['Gas/oil'] * cost
+    result += gs/1000
+    bs = energy['Biofuel'] * cost
+    result += bs/1000
+    neis = energy['Neighboor'] * cost
+    result += neis/1000
+
+    return result
+
+def calculate_revenue(temp, time):
+	cost = 0
+	if 10 <= temp <= 50:
+		if 7 <= time < 11:
+			cost = 0.094
+		elif 11 <= time < 17:
+			cost = 0.134
+		elif 17 <= time < 19:
+			cost = 0.094
+		elif 19 <= time <= 23:
+			cost = 0.065
+		elif 0 <= time < 7:
+			cost = 0.065
+
+	elif -50 <= temp < 10:
+		if 7 <= time < 11:
+			cost = 0.134
+		elif 11 <= time < 17:
+			cost = 0.094
+		elif 17 <= time < 19:
+			cost = 0.134
+		elif 19 <= time <= 23:
+			cost = 0.065
+		elif 0 <= time < 7:
+			cost = 0.065
+	return cost
+
+def update_current_hour(hour):
+	hour += 1
+	return hour
